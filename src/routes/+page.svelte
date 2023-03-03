4465e4a1c2b6ac46fec16db8e04206cbcb5aecbb
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2023-03-03 12:17:29 +0100

    Add styles variables
    Add new styles variables: Colors, Grid, Globals and others utility variables.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<div class="container">
+    <div class="row">
+        <div class="col s6 red">
+            <p>
+                Hola
+            </p>
+        </div>
+        <div class="col s6 blue">
+            <p>
+                Mundo
+            </p>
+        </div>
+    </div>
+</div>
+
 <style lang="scss">
 
 p{
